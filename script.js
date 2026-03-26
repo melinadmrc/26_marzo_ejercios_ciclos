b4d40a39247489edffdee1f3d89a772eb30d1f30
Melina <melina.dmec@gmail.com>
2026-03-26 13:54:13 +0100

    ejercicio 2

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 // FIZZBUZZ
 /*
- * Escribe un programa que muestre por consola (con un print) los
- * números de 1 a 100 (ambos incluidos y con un salto de línea entre
- * cada impresión), sustituyendo los siguientes:
- * - Múltiplos de 3 por la palabra "fizz".
- * - Múltiplos de 5 por la palabra "buzz".
- * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ * - Múltiplos de 3 "fizz".
+ * - Múltiplos de 5 "buzz".
+ * - Múltiplos de 3 y de 5 a la vez "fizzbuzz".
  */
 
 console.log("FIZZBUZZ");
@@ -25,11 +22,8 @@ for (let i = 1; i <= 100; i++) {
 
 // FIBONACCI
 /*
- * Escribe un programa que imprima los 50 primeros números de la sucesión
- * de Fibonacci empezando en 0.
- * - La serie Fibonacci se compone por una sucesión de números en
- *   la que el siguiente siempre es la suma de los dos anteriores.
- *   0, 1, 1, 2, 3, 5, 8, 13...
+ * Imprimir los 50 primeros números de la sucesión de Fibonacci empezando en 0
+ *  sucesión de números en la que el siguiente siempre es la suma de los dos anteriores
  */
 
 console.log("FIBONACCI");
@@ -49,8 +43,7 @@ for (let i = 1; i < 50; i++) {
 // INVERSIÓN DE CADENAS
 /*
  * Crea un programa que invierta el orden de una cadena de texto
- * sin usar funciones propias del lenguaje que lo hagan de forma automática.
- * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ * "Hola mundo" a "odnum aloH"
  */
 
 console.log("INVERSIÓN DE CADENAS");
