4924070c8602c41fe160e93f9ab2dc91e4c6d0f4
Melina <melina.dmec@gmail.com>
2026-03-26 13:41:32 +0100

    ejercicio 1

---
+++ script.js
@@ -0,0 +1,65 @@
+// FIZZBUZZ
+/*
+ * Escribe un programa que muestre por consola (con un print) los
+ * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+ * cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ */
+
+console.log("FIZZBUZZ");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+
+// FIBONACCI
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+
+console.log("FIBONACCI");
+
+let anterior = 0;
+let actual = 1;
+
+console.log(anterior);
+
+for (let i = 1; i < 50; i++) {
+  console.log(actual);
+  let siguiente = anterior + actual;
+  anterior = actual;
+  actual = siguiente;
+}
+
+// INVERSIÓN DE CADENAS
+/*
+ * Crea un programa que invierta el orden de una cadena de texto
+ * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+ * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+ */
+
+console.log("INVERSIÓN DE CADENAS");
+
+let cadena = "Hola mundo";
+let resultado = "";
+
+for (let i = cadena.length - 1; i >= 0; i--) {
+  resultado = resultado + cadena[i];
+}
+
+console.log(resultado);
